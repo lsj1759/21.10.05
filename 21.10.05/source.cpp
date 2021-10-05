82d2dfd2dac6e18d6713ef4ae10538723bb23a46
lsj1759 <lsj1759@naver.com>
2021-10-05 11:29:14 +0900

    class

---
+++ 21.10.05/source.cpp
@@ -0,0 +1,59 @@
+#include <string>
+
+using namespace std;
+
+class Character
+{
+public:
+	string Id;
+	string Name;
+	int Str;
+	int Dex;
+};
+
+int main()
+{
+	Character Characters[3];
+
+	Characters[0].Id = "hong";
+	Characters[0].Name = "È«±æµ¿";
+	Characters[0].Str = 100;
+	Characters[0].Dex = 90;
+
+	Characters[1].Id = "hong2";
+	Characters[1].Name = "È«±æ¼ø";
+	Characters[1].Str = 10;
+	Characters[1].Dex = 10;
+
+	Characters[2].Id = "ko";
+	Characters[2].Name = "°í±æµ¿";
+	Characters[2].Str = 10;
+	Characters[2].Dex = 10;
+}
+
+class Image
+{
+public:
+	int X;
+	int Y;
+	int R;
+	int G;
+	int B;
+};
+
+int main()
+{
+	Image images[2];
+
+	images[0].X = 0;
+	images[0].Y = 0;
+	images[0].R = 165;
+	images[0].G = 55;
+	images[0].B = 128;
+
+	images[1].X = 0;
+	images[1].Y = 1;
+	images[1].R = 133;
+	images[1].G = 28;
+	images[1].B = 182;
+}
